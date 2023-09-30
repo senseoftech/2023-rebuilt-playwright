48e4e8eb0be2f2338cd9339f573706aa2f0d6e5b
Adrien Clerbois <adrien.clerbois@outlook.com>
2023-09-30 18:12:03 +0200

    Add fake tests

--- src/Sot.Rebuild.Tests/UnitTest1.cs
+++ src/Sot.Rebuild.Tests/UnitTest1.cs
@@ -3,8 +3,38 @@ namespace Sot.Rebuild.Tests;
 [TestClass]
 public class UnitTest1
 {
-    [TestMethod]
-    public void TestMethod1()
+    [DataRow(0)]
+    [DataRow(1)]
+    [DataRow(2)]
+    [DataRow(3)]
+    [DataRow(4)]
+    [DataRow(5)]
+    [DataRow(6)]
+    [DataRow(7)]
+    [DataRow(8)]
+    [DataRow(9)]
+    [DataRow(10)]
+    [DataRow(11)]
+    [DataRow(12)]
+    [DataRow(13)]
+    [DataRow(14)]
+    [DataRow(15)]
+    [DataRow(16)]
+    [DataRow(17)]
+    [DataRow(18)]
+    [DataRow(19)]
+    [DataRow(20)]
+    [DataRow(21)]
+    [DataRow(22)]
+    [DataRow(23)]
+    [DataRow(24)]
+    [DataRow(25)]
+    [DataRow(26)]
+    [DataRow(27)]
+    [DataRow(28)]
+    [DataRow(29)]
+    [DataTestMethod]
+    public void FakeUnitTest(int number)
     {
     }
 }

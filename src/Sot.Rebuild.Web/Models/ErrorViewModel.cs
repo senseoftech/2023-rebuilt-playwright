cd609e42af5cd6477be118268a4b69b6e925bfda
Adrien Clerbois <adrien.clerbois@outlook.com>
2023-09-30 11:46:35 +0200

    Remove nullable warning

--- src/Sot.Rebuild.Web/Models/ErrorViewModel.cs
+++ src/Sot.Rebuild.Web/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace Sot.Rebuild.Web.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
